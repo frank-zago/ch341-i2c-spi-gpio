35ae155830df3fb9c25795c15b47fc02d085e592
frank zago <frank@zago.net>
2023-04-29 22:37:12 -0500

    Rework i2c part to use one segment per transfer
    Using requests with multiple 32-byte segments doesn't always
    work. Split very transaction in segments of 32 bytes. For the writes,
    check that devce chip is ready to process it first. For the read, ask
    a status byte for every segemnt.
    
    This fixes 24C eeprom writes, as well as i2cdetect.
    
      # i2cdetect -r 1
      WARNING! This program can confuse your I2C bus, cause data loss and worse!
      I will probe file /dev/i2c-1 using receive byte commands.
      I will probe address range 0x08-0x77.
      Continue? [Y/n]
           0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
      00:                         -- -- -- -- -- -- -- --
      10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      50: 50 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      70: -- -- -- -- -- -- -- --
    
      # i2cdetect -q 1
      WARNING! This program can confuse your I2C bus, cause data loss and worse!
      I will probe file /dev/i2c-1 using quick write commands.
      I will probe address range 0x08-0x77.
      Continue? [Y/n]
           0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
      00:                         -- -- -- -- -- -- -- --
      10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      50: 50 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
      70: -- -- -- -- -- -- -- --
    
    Issue #13, based on PR #7.

--- i2c-ch341.c
+++ i2c-ch341.c
@@ -32,36 +32,21 @@
 #define CH341_CMD_I2C_STM_IN 0xC0
 #define CH341_CMD_I2C_STM_SET 0x60
 
-/*
- * The maximum request size is 4096 bytes, both for reading and
- * writing, split in up to 128 32-byte segments, which includes the
- * CH341 commands and the I2C data.
- */
-#define SEG_COUNT 128
-
-/*
- * Limit the transfer size that can be written. 4KiB is the maximum
- * size of the whole buffer, but it must include all the command
- * delimiters. 3KiB sounds reasonable.
- */
-#define MAX_RW_LENGTH 3072
+/* Limit the transfer read size in one go to 32 bytes. */
+#define MAX_RD_LENGTH 32
 
 struct ch341_i2c {
 	struct i2c_adapter adapter;
-
-	/* I2C request and response state */
-	int idx_out;		/* current offset in buf */
-	int out_seg;		/* current segment */
 	u8 *i2c_buf;
 };
 
 /*
- * Special case the no data transfer (SMBUS quick message), since some
- * parameters are a bit different than the normal case.
+ * Check whether the device is busy. The CH341 sets a bit in the first
+ * byte if it cannot process a new command yet.
  */
-static int no_data_xfer(struct i2c_adapter *adapter, const struct i2c_msg *msg)
+static int is_dev_busy(const struct i2c_adapter *adapter, const struct i2c_msg *msg)
 {
-	struct ch341_i2c *dev = i2c_get_adapdata(adapter);
+	const struct ch341_i2c *dev = i2c_get_adapdata(adapter);
 	struct ch341_ddata *ddata = adapter->algo_data;
 	u8 *out = dev->i2c_buf;
 	int actual;
@@ -70,160 +55,161 @@ static int no_data_xfer(struct i2c_adapter *adapter, const struct i2c_msg *msg)
 	out[0] = CH341_CMD_I2C_STREAM;
 	out[1] = CH341_CMD_I2C_STM_STA;
 	out[2] = CH341_CMD_I2C_STM_OUT;
-	out[3] = msg->addr << 1 | (msg->flags & I2C_M_RD);
-	out[4] = CH341_CMD_I2C_STM_IN;
-	out[5] = CH341_CMD_I2C_STM_STO;
-	out[6] = CH341_CMD_I2C_STM_END;
+	out[3] = msg->addr << 1;
+	out[4] = CH341_CMD_I2C_STM_STO;
+	out[5] = CH341_CMD_I2C_STM_END;
 
 	mutex_lock(&ddata->usb_lock);
 
-	/* Issue the request */
 	ret = usb_bulk_msg(ddata->usb_dev,
 			   usb_sndbulkpipe(ddata->usb_dev, ddata->ep_out),
-			   dev->i2c_buf, 7, &actual, DEFAULT_TIMEOUT_MS);
-	if (ret < 0) {
-		mutex_unlock(&ddata->usb_lock);
-		return ret;
-	}
-
-	ret = usb_bulk_msg(ddata->usb_dev,
-			   usb_rcvbulkpipe(ddata->usb_dev, ddata->ep_in),
-			   dev->i2c_buf, 2, &actual,
-			   DEFAULT_TIMEOUT_MS);
-	if (ret < 0) {
-		mutex_unlock(&ddata->usb_lock);
-		return ret;
+			   dev->i2c_buf, 6, &actual, DEFAULT_TIMEOUT_MS);
+	if (ret == 0) {
+		ret = usb_bulk_msg(ddata->usb_dev,
+				   usb_rcvbulkpipe(ddata->usb_dev, ddata->ep_in),
+				   dev->i2c_buf, SEG_SIZE, &actual,
+				   DEFAULT_TIMEOUT_MS);
 	}
 
 	mutex_unlock(&ddata->usb_lock);
 
-	if (actual != 2 || dev->i2c_buf[0] & 0x80)
-		return -ETIMEDOUT;
+	if (ret == 0 && (actual != 1 || dev->i2c_buf[0] & 0x80))
+		ret = -ETIMEDOUT;
 
-	return 1;
+	return ret;
 }
 
-/*
- * Append a write command to the current request. A set of 32-byte
- * packets is filled. Each packet starts with STREAM and finishes with
- * END, and contains an OUT field, leaving up to 29 bytes of data. The
- * first packet must also include a START and the device address.
- */
-static int append_write(struct ch341_i2c *dev, const struct i2c_msg *msg)
+static int ch341_i2c_write(const struct i2c_adapter *adapter, const struct i2c_msg *msg)
 {
+	const struct ch341_i2c *dev = i2c_get_adapdata(adapter);
+	struct ch341_ddata *ddata = adapter->algo_data;
+	const u8 *src = msg->buf;
 	bool start_done = false;
-	u8 *out = dev->i2c_buf;
-	int len;
-	u8 *p;
+	bool stop_done = false;
+	unsigned int len;
+	int actual;
+	int ret;
+
+	ret = is_dev_busy(adapter, msg);
+	if (ret)
+		return ret;
 
 	len = msg->len;
-	p = msg->buf;
 
-	while (len) {
-		int to_write;
-		int avail;
+	while (!stop_done) {
+		u8 *out = dev->i2c_buf;
+		unsigned int to_write;
 
-		if (dev->idx_out % SEG_SIZE) {
-			/* Finish current packet, and advance to the next one */
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_END;
-			dev->out_seg++;
-			dev->idx_out = dev->out_seg * SEG_SIZE;
+		*out++ = CH341_CMD_I2C_STREAM;
 
-			if (dev->out_seg == SEG_COUNT)
-				return -E2BIG;
-		}
-
-		out[dev->idx_out++] = CH341_CMD_I2C_STREAM;
-
-		/* account for stream start and end */
-		avail = SEG_SIZE - 3;
-
-		if (!start_done) {
-			/* Each message has a start */
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_STA;
-
-			avail -= 2; /* room for STA and device address */
-		}
-
-		to_write = min_t(int, len, avail);
-
-		if (!start_done) {
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_OUT | (to_write + 1);
-			out[dev->idx_out++] = msg->addr << 1;
+		if (start_done)  {
+			to_write = min_t(unsigned int, len, SEG_SIZE - 3);
+			if (to_write)
+				*out++ = CH341_CMD_I2C_STM_OUT | to_write;
+		} else {
+			to_write = min_t(unsigned int, len, SEG_SIZE - 5);
+			*out++ = CH341_CMD_I2C_STM_STA;
+			*out++ = CH341_CMD_I2C_STM_OUT | (to_write + 1);
+			*out++ = msg->addr << 1;
 
 			start_done = true;
-		} else {
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_OUT | to_write;
 		}
 
-		memcpy(&out[dev->idx_out], p, to_write);
-		dev->idx_out += to_write;
+		memcpy(out, src, to_write);
+		out += to_write;
+		src += to_write;
 		len -= to_write;
-		p += to_write;
+
+		/* Corner case. There may not be enough room to add
+		 * the i2c STOP in this packet. Add it to the next
+		 * (empty) packet.
+		 */
+		if (len == 0 && ((out - dev->i2c_buf) < (SEG_SIZE - 1))) {
+			*out++ = CH341_CMD_I2C_STM_STO;
+			stop_done = true;
+		}
+
+		*out++ = CH341_CMD_I2C_STM_END;
+
+		mutex_lock(&ddata->usb_lock);
+
+		ret = usb_bulk_msg(ddata->usb_dev,
+				   usb_sndbulkpipe(ddata->usb_dev, ddata->ep_out),
+				   dev->i2c_buf, out - dev->i2c_buf, &actual, DEFAULT_TIMEOUT_MS);
+
+		mutex_unlock(&ddata->usb_lock);
+
+		if (ret < 0)
+			return ret;
 	}
 
 	return 0;
 }
 
-/*
- * Append a read command to the request. It usually follows a write
- * command. When that happens, the driver will attempt to concat the
- * read command into the same packet.  Each read command, of up to 32
- * bytes, must be written to a new packet. It is not possible to
- * concat them.
- */
-static int append_read(struct ch341_i2c *dev, const struct i2c_msg *msg)
+static int ch341_i2c_read(const struct i2c_adapter *adapter, const struct i2c_msg *msg)
 {
-	bool start_done = false;
-	u8 *out = dev->i2c_buf;
-	int len;
+	struct ch341_i2c *dev = i2c_get_adapdata(adapter);
+	struct ch341_ddata *ddata = adapter->algo_data;
+	u8 *dst = msg->buf;
+	unsigned int len;
+	int actual;
+	int ret;
 
 	len = msg->len;
 
 	while (len) {
-		int to_read;
+		u8 *out = dev->i2c_buf;
+		unsigned int to_read;
 
-		if (dev->idx_out % SEG_SIZE) {
-			if (!start_done &&
-			    (dev->idx_out % SEG_SIZE) <  (SEG_SIZE - 7)) {
-				/* There's enough left for a read */
-			} else {
-				/* Finish current packet, and advance to the next one */
-				out[dev->idx_out++] = CH341_CMD_I2C_STM_END;
-				dev->out_seg++;
-				dev->idx_out = dev->out_seg * SEG_SIZE;
+		to_read = min_t(unsigned int, len, SEG_SIZE - 1);
 
-				if (dev->out_seg == SEG_COUNT)
-					return -E2BIG;
+		mutex_lock(&ddata->usb_lock);
 
-				out[dev->idx_out++] = CH341_CMD_I2C_STREAM;
-			}
-		} else {
-			out[dev->idx_out++] = CH341_CMD_I2C_STREAM;
-		}
+		*out++ = CH341_CMD_I2C_STREAM;
+		*out++ = CH341_CMD_I2C_STM_STA;
+		*out++ = CH341_CMD_I2C_STM_OUT;  /* request status byte */
+		*out++ = (msg->addr << 1) | 1;
 
-		if (!start_done) {
-			/* Each message has a start */
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_STA;
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_OUT | 1;
-			out[dev->idx_out++] = msg->addr << 1 | 1;
-
-			start_done = true;
-		}
-
-		/* Apparently the last command must be an STM_IN to
-		 * read the last byte. Without it, the adapter gets
+		/* The last command must be an STM_IN to read
+		 * the last byte. Without it, the adapter gets
 		 * lost.
 		 */
-		to_read = min_t(int, len, 32);
-		len -= to_read;
-		if (len == 0) {
-			if (to_read > 1)
-				out[dev->idx_out++] = CH341_CMD_I2C_STM_IN | (to_read - 1);
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_IN;
-		} else {
-			out[dev->idx_out++] = CH341_CMD_I2C_STM_IN | to_read;
+		if (to_read > 1)
+			*out++ = CH341_CMD_I2C_STM_IN | (to_read - 1);
+		*out++ = CH341_CMD_I2C_STM_IN;
+
+		*out++ = CH341_CMD_I2C_STM_STO;
+		*out++ = CH341_CMD_I2C_STM_END;
+
+		/* Issue the request */
+		ret = usb_bulk_msg(ddata->usb_dev,
+				   usb_sndbulkpipe(ddata->usb_dev, ddata->ep_out),
+				   dev->i2c_buf, out - dev->i2c_buf, &actual, DEFAULT_TIMEOUT_MS);
+		if (ret < 0) {
+			mutex_unlock(&ddata->usb_lock);
+			return ret;
 		}
+
+		ret = usb_bulk_msg(ddata->usb_dev,
+				   usb_rcvbulkpipe(ddata->usb_dev, ddata->ep_in),
+				   dev->i2c_buf, to_read + 1, &actual,
+				   DEFAULT_TIMEOUT_MS);
+		if (ret) {
+			mutex_unlock(&ddata->usb_lock);
+			return ret;
+		}
+
+		if (actual < 1 || dev->i2c_buf[0] & 0x80) {
+			mutex_unlock(&ddata->usb_lock);
+			return -ETIMEDOUT;
+		}
+
+		memcpy(dst, &dev->i2c_buf[1], to_read);
+
+		mutex_unlock(&ddata->usb_lock);
+
+		len -= to_read;
+		dst += to_read;
 	}
 
 	return 0;
@@ -231,81 +217,20 @@ static int append_read(struct ch341_i2c *dev, const struct i2c_msg *msg)
 
 static int ch341_i2c_xfer(struct i2c_adapter *adapter, struct i2c_msg *msgs, int num)
 {
-	struct ch341_i2c *dev = i2c_get_adapdata(adapter);
-	struct ch341_ddata *ddata = adapter->algo_data;
-	u8 *out = dev->i2c_buf;
-	int actual;
 	int ret;
 	int i;
 
-	/* Special case */
-	if (num == 1 && msgs[0].len == 0)
-		return no_data_xfer(adapter, &msgs[0]);
-
-	/* Prepare the request */
-	dev->idx_out = 0;
-	dev->out_seg = 0;
-
 	for (i = 0; i != num; i++) {
-		if (msgs[i].flags & I2C_M_RD)
-			ret = append_read(dev, &msgs[i]);
+		struct i2c_msg *msg = &msgs[i];
+
+		if (msg->flags & I2C_M_RD)
+			ret = ch341_i2c_read(adapter, msg);
 		else
-			ret = append_write(dev, &msgs[i]);
+			ret = ch341_i2c_write(adapter, msg);
 
 		if (ret)
 			return ret;
 	}
-
-	/* Finish the last packet */
-	if (SEG_SIZE - (dev->idx_out % SEG_SIZE) < 2) {
-		out[dev->idx_out++] = CH341_CMD_I2C_STM_END;
-
-		dev->out_seg++;
-		if (dev->out_seg == SEG_COUNT)
-			return -E2BIG;
-
-		dev->idx_out = dev->out_seg * SEG_SIZE;
-
-		out[dev->idx_out++] = CH341_CMD_I2C_STREAM;
-	}
-
-	out[dev->idx_out++] = CH341_CMD_I2C_STM_STO;
-	out[dev->idx_out++] = CH341_CMD_I2C_STM_END;
-
-	mutex_lock(&ddata->usb_lock);
-
-	/* Issue the request */
-	ret = usb_bulk_msg(ddata->usb_dev,
-			   usb_sndbulkpipe(ddata->usb_dev, ddata->ep_out),
-			   dev->i2c_buf, dev->idx_out, &actual, DEFAULT_TIMEOUT_MS);
-	if (ret < 0) {
-		mutex_unlock(&ddata->usb_lock);
-		return ret;
-	}
-
-	for (i = 0; i != num; i++) {
-		if (!(msgs[i].flags & I2C_M_RD))
-			continue;
-
-		ret = usb_bulk_msg(ddata->usb_dev,
-				  usb_rcvbulkpipe(ddata->usb_dev, ddata->ep_in),
-				  dev->i2c_buf, msgs[i].len, &actual,
-				  DEFAULT_TIMEOUT_MS);
-
-		if (ret) {
-			mutex_unlock(&ddata->usb_lock);
-			return ret;
-		}
-
-		if (actual != msgs[i].len) {
-			mutex_unlock(&ddata->usb_lock);
-			return -EIO;
-		}
-
-		memcpy(msgs[i].buf, dev->i2c_buf, actual);
-	}
-
-	mutex_unlock(&ddata->usb_lock);
 
 	return num;
 }
@@ -321,8 +246,7 @@ static const struct i2c_algorithm ch341_i2c_algorithm = {
 };
 
 static const struct i2c_adapter_quirks ch341_i2c_quirks = {
-	.max_read_len = MAX_RW_LENGTH,
-	.max_write_len = MAX_RW_LENGTH,
+	.max_read_len = MAX_RD_LENGTH,
 };
 
 static int ch341_i2c_probe(struct platform_device *pdev)
@@ -336,7 +260,7 @@ static int ch341_i2c_probe(struct platform_device *pdev)
 	if (ch341_i2c == NULL)
 		return -ENOMEM;
 
-	ch341_i2c->i2c_buf = devm_kzalloc(&pdev->dev, SEG_COUNT * SEG_SIZE, GFP_KERNEL);
+	ch341_i2c->i2c_buf = devm_kzalloc(&pdev->dev, SEG_SIZE, GFP_KERNEL);
 	if (ch341_i2c->i2c_buf == NULL)
 		return -ENOMEM;
 
