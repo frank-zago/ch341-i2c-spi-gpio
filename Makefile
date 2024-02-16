frank-zago/ch341-i2c-spi-gpio

commit f7a24cac843dd19f49772969a3c644bd8e4bcb1a

Added DKMS support

Makefile
@@ -1,6 +1,9 @@
-PWD         := $(shell pwd)
+PWD := $(shell pwd)
 KVERSION    := $(shell uname -r)
-KDIR        ?= /lib/modules/$(KVERSION)/build
+KDIR        := /lib/modules/$(KVERSION)/build
+DKMS       := $(shell which dkms 2> /dev/null)
+MODULE_DIR  := /lib/modules/$(KVERSION)
+
 
 obj-m += ch341-core.o
 obj-m += i2c-ch341.o
@@ -11,3 +14,24 @@ all:
 	make -C $(KDIR) M=$(PWD) modules
 clean:
 	make -C $(KDIR) M=$(PWD) clean
+
+ifeq ($(DKMS),)  # if DKMS is not installed
+install: all
+	make -C $(KDIR) M=$(PWD) modules_install
+	depmod
+else
+# Package version and name from dkms.conf
+VER := $(shell sed -n 's/^PACKAGE_VERSION=\([^\n]*\)/\1/p' dkms.conf 2> /dev/null)
+MODULE_NAME := $(shell sed -n 's/^PACKAGE_NAME=\([^\n]*\)/\1/p' dkms.conf 2> /dev/null)
+MODULE_INSTALLED := $(shell dkms status $(MODULE_NAME))
+install: all
+ifneq ($(MODULE_INSTALLED),)
+	echo Module $(MODULE_NAME) is installed ... uninstall it first
+	make uninstall
+endif
+	cp -R . /usr/src/$(MODULE_NAME)-$(VER)
+	dkms install -m $(MODULE_NAME) -v $(VER)
+uninstall:
+	dkms remove -m $(MODULE_NAME) -v $(VER) --all
+	rm -rf /usr/src/$(MODULE_NAME)-$(VER)
+endif

dkms.conf
@@ -1,0 +1,23 @@
+PACKAGE_NAME=ch341-i2c-spi-gpio
+PACKAGE_VERSION="1.0.0"
+
+MAKE[0]="make -C ${kernel_source_dir} M=${dkms_tree}/${PACKAGE_NAME}/${PACKAGE_VERSION}/build modules"
+CLEAN="make -C ${kernel_source_dir} M=${dkms_tree}/${PACKAGE_NAME}/${PACKAGE_VERSION}/build clean"
+
+DEST_MODULE_LOCATION[0]=/extra
+BUILT_MODULE_NAME[0]="ch341-core"
+BUILT_MODULE_LOCATION[0]="./"
+
+DEST_MODULE_LOCATION[1]=/extra
+BUILT_MODULE_NAME[1]="i2c-ch341"
+BUILT_MODULE_LOCATION[1]="./"
+
+DEST_MODULE_LOCATION[2]=/extra
+BUILT_MODULE_NAME[2]="gpio-ch341"
+BUILT_MODULE_LOCATION[2]="./"
+
+DEST_MODULE_LOCATION[3]=/extra
+BUILT_MODULE_NAME[3]="spi-ch341"
+BUILT_MODULE_LOCATION[3]="./"
+
+AUTOINSTALL="yes"

commit 1f4113a4536edaabf017e5caeaf4f20c72536365

Restore KDIR functionnality
Don't overwrite KDIR if it was set in the environment. This was broken
in commit f7a24cac843dd19f49772969a3c644bd8e4bcb1a "Added DKMS
support".

Makefile
@@ -1,7 +1,7 @@
 PWD := $(shell pwd)
 KVERSION    := $(shell uname -r)
 ifeq ($(KERNEL_SRC),) # KERNEL_SRC is set to kernel src dir in Yocto Linux build
-    KDIR    := /lib/modules/$(KVERSION)/build
+    KDIR    ?= /lib/modules/$(KVERSION)/build
 else
     KDIR    ?= $(KERNEL_SRC)
 endif