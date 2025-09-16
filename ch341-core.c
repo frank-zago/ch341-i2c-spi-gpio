872e0fb6e9dd9dd2711a7e267098f715bd0da9b4
Vincent Jardin <vjardin@free.fr>
2025-09-16 21:30:04 +0200

    probing: better handle USB probing
    Let's improve the former commit in order to handle more USB devices
    to be added later on.

--- ch341-core.c
+++ ch341-core.c
@@ -21,26 +21,79 @@
 #define CH341_USB_EP_INT   0x01
 #define CH341_USB_EP_BULK  0x02
 
+#define USB_DIR(d) \
+	((d) == USB_DIR_OUT ? 0 : \
+	 (d) == USB_DIR_IN ? 1  : \
+	 0)
+
+struct ch341_ep_map {
+	u8 bulk[2]; /* [O] out, [1] in */
+	u8 intr[2]; /* [0] out, [1] in */
+};
+
 static const struct mfd_cell ch341_devs[] = {
 	{ .name = "ch341-gpio", },
 	{ .name = "ch341-i2c", },
 };
 
+/* helper for probing */
+static const struct usb_endpoint_descriptor *
+ch341_find_ep(const struct usb_host_interface *alt, u8 dir, u8 xfertype,
+              const struct ch341_ep_map *map)
+{
+	for (int i = 0; i < alt->desc.bNumEndpoints; i++) {
+		const struct usb_endpoint_descriptor *ep = &alt->endpoint[i].desc;
+
+		if (dir == USB_DIR_IN &&
+		    !usb_endpoint_dir_in(ep))
+			continue;
+		if (dir == USB_DIR_OUT &&
+		    !usb_endpoint_dir_out(ep))
+			continue;
+
+		if (map) {
+			u8 want = 0;
+
+			switch (xfertype) {
+			case USB_ENDPOINT_XFER_BULK:
+				want = map->bulk[USB_DIR(dir)];
+				break;
+			case USB_ENDPOINT_XFER_INT:
+				want = map->intr[USB_DIR(dir)];
+				break;
+			case USB_ENDPOINT_XFER_CONTROL:
+				break;
+			}
+
+			/* "don’t care" */
+			if (want && ep->bEndpointAddress != want)
+				continue;
+		}
+
+		if ((xfertype == USB_ENDPOINT_XFER_BULK    && usb_endpoint_xfer_bulk(ep)) ||
+		    (xfertype == USB_ENDPOINT_XFER_INT     && usb_endpoint_xfer_int(ep))  ||
+		    (xfertype == USB_ENDPOINT_XFER_CONTROL && usb_endpoint_xfer_control(ep)))
+			return ep;
+
+	}
+	return NULL;
+}
+
 static int ch341_usb_probe(struct usb_interface *iface,
 			   const struct usb_device_id *usb_id)
 {
-	struct usb_endpoint_descriptor *bulk_out = NULL;
-	struct usb_endpoint_descriptor *bulk_in = NULL;
-	struct usb_endpoint_descriptor *intr_in = NULL;
+	const struct ch341_ep_map *map = (const void *)usb_id->driver_info;
+	const struct usb_host_interface *alt = iface->cur_altsetting;
+	const struct usb_endpoint_descriptor *bulk_out = NULL;
+	const struct usb_endpoint_descriptor *bulk_in = NULL;
+	const struct usb_endpoint_descriptor *intr_in = NULL;
 	struct ch341_ddata *ddata;
 	int ret;
-	static const u8 int_ep_addr[] = {
-		CH341_USB_EP_INT | USB_DIR_IN,
-		0};
-	static const u8 bulk_ep_addr[] = {
-		CH341_USB_EP_BULK | USB_DIR_IN,
-		CH341_USB_EP_BULK | USB_DIR_OUT,
-		0};
+
+	if (alt->desc.bInterfaceClass != USB_CLASS_VENDOR_SPEC) {
+		dev_warn(&iface->dev, "Not a vendor-specific interface; skip\n");
+		return -ENODEV;
+	}
 
 	ddata = devm_kzalloc(&iface->dev, sizeof(*ddata), GFP_KERNEL);
 	if (!ddata)
@@ -49,28 +102,12 @@ static int ch341_usb_probe(struct usb_interface *iface,
 	ddata->usb_dev = interface_to_usbdev(iface);
 	mutex_init(&ddata->usb_lock);
 
-	if (!usb_check_bulk_endpoints(iface, bulk_ep_addr) ||
-	    !usb_check_int_endpoints(iface, int_ep_addr)) {
-		dev_err(&iface->dev, "Expected EPs 0x%02x/0x%02x (bulk) and 0x%02x (int) not found\n",
-		        CH341_USB_EP_BULK | USB_DIR_IN, CH341_USB_EP_BULK | USB_DIR_OUT,
-		        CH341_USB_EP_INT | USB_DIR_IN);
+	bulk_in  = ch341_find_ep(alt, USB_DIR_IN, USB_ENDPOINT_XFER_BULK, map);
+	bulk_out = ch341_find_ep(alt, USB_DIR_OUT, USB_ENDPOINT_XFER_BULK, map);
+	intr_in  = ch341_find_ep(alt, USB_DIR_IN, USB_ENDPOINT_XFER_INT, map);
+	if (!bulk_in || !bulk_out || !intr_in) {
+		dev_err(&iface->dev, "Missing some required endpoints (bulk-in/out or int-in)\n");
 		return -ENODEV;
-	}
-
-	ret = usb_find_common_endpoints(iface->cur_altsetting, &bulk_in,
-					&bulk_out, &intr_in, NULL);
-	if (ret) {
-		dev_err(&iface->dev, "Could not find all endpoints\n");
-		return -ENODEV;
-	}
-
-	if (bulk_in->bEndpointAddress  != (CH341_USB_EP_BULK | USB_DIR_IN)  ||
-	    bulk_out->bEndpointAddress != (CH341_USB_EP_BULK | USB_DIR_OUT) ||
-	    intr_in->bEndpointAddress  != (CH341_USB_EP_INT  | USB_DIR_IN)) {
-	    dev_err(&iface->dev, "Unexpected EP addrs: in 0x%02x out 0x%02x int 0x%02x\n",
-	            bulk_in->bEndpointAddress, bulk_out->bEndpointAddress,
-	            intr_in->bEndpointAddress);
-	    return -ENODEV;
 	}
 
 	ddata->ep_in = bulk_in->bEndpointAddress;
@@ -85,6 +122,7 @@ static int ch341_usb_probe(struct usb_interface *iface,
 	if (ret)
 		dev_err(&iface->dev, "Failed to add child devices\n");
 
+
 	return ret;
 }
 
@@ -93,8 +131,18 @@ static void ch341_usb_disconnect(struct usb_interface *usb_if)
 
 }
 
+static const struct ch341_ep_map map_1a86_5512 = {
+	.bulk[USB_DIR(USB_DIR_IN)] = CH341_USB_EP_BULK | USB_DIR_IN,
+	.bulk[USB_DIR(USB_DIR_OUT)] = CH341_USB_EP_BULK | USB_DIR_OUT,
+	.intr[USB_DIR(USB_DIR_IN)] = CH341_USB_EP_INT | USB_DIR_IN,
+};
+
 static const struct usb_device_id ch341_usb_table[] = {
-	{ USB_DEVICE(0x1a86, 0x5512) },
+	{
+		USB_DEVICE_AND_INTERFACE_INFO(0x1a86, 0x5512,
+		USB_CLASS_VENDOR_SPEC, 0x01, 0x02), /* bInterfaceSubClass, bInterfaceProtocol */
+		.driver_info = (kernel_ulong_t)&map_1a86_5512, // optional, but nice to set
+	},
 	{ }
 };
 MODULE_DEVICE_TABLE(usb, ch341_usb_table);
